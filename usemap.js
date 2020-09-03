a8d55e682748aaaecee0db8765e100bb4c7b761d
Oleksiy Oletsky <oletsky@ukr.net>
2020-09-03 13:56:10 +0300

    Add files via upload

---
+++ usemap.js
@@ -0,0 +1,12 @@
+let coll=new Map();
+coll.set("bread",100);
+coll.set("whiskey",20);
+coll.set("water",1000);
+console.log(coll);
+coll.set("water",2000);
+coll.delete("whiskey");
+console.log(coll.get("water"));
+//By for-of
+for (let [key, value] of coll.entries()) {
+    console.log(key + ' - ' + value);
+}
---
+++ useofset.js
@@ -0,0 +1,7 @@
+let mySet=new Set();
+mySet.add(10);
+mySet.add(20);
+mySet.add(10);
+for(let v of mySet) {
+    console.log(v);
+}
